d540f3ce8c54a586312f008e3ab44126032d8f26
lucyr233 <rhxlucy@gmail.com>
2017-08-18 14:35:08 -0400

    add headway calculation function

--- readinreport.py
+++ readinreport.py
@@ -29,8 +29,9 @@ def turnintotable(content):
                 try1['route']=fieldroute
                 try1['Direction']=fieldirection
                 try1['stop']=fieldstop
+                clear=caltable(try1)
                 #print(try1)
-                tryall=tryall.append(try1,ignore_index=True)
+                tryall=tryall.append(clear,ignore_index=True)
                 try1=pd.DataFrame()
             else:
                 fields=line.split(',')
@@ -52,11 +53,12 @@ def time_diff(start, end):
         assert isinstance(end, datetime_time)
         start, end = [datetime.combine(datetime.min, t) for t in [start, end]]
     if start <= end: # e.g., 10:33:26-11:15:49
-        return end - start
+        diff=end - start
     else: # end < start e.g., 23:55:00-00:25:00
         end += timedelta(1) # +day
         assert end > start
-        return end - start
+        diff=end - start
+    return diff.seconds/60
 
 
 # In[4]:
@@ -76,6 +78,29 @@ for fname in glob.glob(path):
     tableall=tableall.append(table,ignore_index=True)
 
 
+def caltable(table):
+    table['date']=table['date'].apply(int)
+    
+    sortsch=table.sort_values(['Scheduled']).reset_index(drop=True)
+    sortsch['diffsch']=''
+    for i in range(len(sortsch)-1):
+        i=i+1
+        sortsch['diffsch'][i]=int(time_diff(sortsch['Scheduled'][i-1],sortsch['Scheduled'][i]))
+    
+    sortsch['diffact']=''
+    sortact=sortsch.sort_values(['Actual']).reset_index(drop=True)
+    for i in range(len(sortact)-1):
+        i=i+1
+        sortact['diffact'][i]=round(time_diff(sortact['Actual'][i-1],sortact['Actual'][i]))
+    
+    for i in range(len(sortact['Scheduled'])):
+        sortact['Scheduled'][i]=datetime.strptime(sortact['Scheduled'][i], '%H:%M:%S').time()
+        
+    print('one stop one day clean')
+    return sortact    
+    
+    
+'''    
 # In[21]:
 
 sorttry=tableall.sort_values(['stop','Direction','date','Scheduled']).reset_index(drop=True)
@@ -115,21 +140,21 @@ for i in range(len(sortact['Scheduled'])):
 
 sortact['diffsch_min'][1::]=sortact['diffsch_min'][1::].apply(int)
 
-
+'''
 # In[55]:
 
 A=sortact[1::].loc[sortact['date']=='01']
 pp.plot(range(len(A.loc[A['diffsch_min']<=50]['Scheduled'])),A.loc[A['diffsch_min']<=50]['diffsch_min'])
 #pp.plot(range(len(sortact.loc[sortact['date']=='01']['Scheduled'])-1),sortact.loc[sortact['date']=='01']['diffsch_min'][1::])
 
-
+'''
 # In[63]:
 
 sortact['diffact_min']=''
 for i in range(len(sortact)-1):
     i=i+1
     sortact['diffact_min'][i]=round(sortact['diffact'][i].seconds/60)
-
+'''
 
 # In[66]:
 
